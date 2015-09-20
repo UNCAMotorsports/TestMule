1cc7a12126448580b5fb16c3a9102eafbd224d26
Wetmelon <pfguenette@gmail.com>
2015-09-20 00:02:00 -0400

    Teensy changes, working code

--- code/TestMule/TestMule.ino
+++ code/TestMule/TestMule.ino
@@ -2,40 +2,56 @@
 #include "DAC_MCP49xx.h"
 
 #define CS_DAC0 10
-#define CS_DAC1 8
-#define THROTTLE_PIN A2
-#define STEERING_PIN A5
-#define LATCH_PIN 6
+#define CS_DAC1 9
+#define THROTTLE_PIN A0
+#define STEERING_PIN A1
+#define LATCH_PIN 4
+#define ENC_TO_RPM 150000
 
 DAC_MCP49xx dac0(DAC_MCP49xx::MCP4921, CS_DAC0);
 DAC_MCP49xx dac1(DAC_MCP49xx::MCP4921, CS_DAC1);
 
 uint32_t lastTime, thisTime;
 uint32_t leftRPM, rightRPM;
-uint32_t leftThrottle, rightThrottle;
+uint16_t leftThrottle, rightThrottle;
 uint32_t throttlePos, steeringPos;
 
 volatile uint32_t APulses;
 volatile uint32_t BPulses;
 
+double leftSteer;
+double rightSteer;
+
 void setup()
 {
-	Serial.begin(115200);
-	delay(1000);
-
-	pinMode(2, INPUT);
-	pinMode(3, INPUT);
+	pinMode(CS_DAC0, OUTPUT);
+	pinMode(CS_DAC1, OUTPUT);
+	pinMode(11, OUTPUT);
 	pinMode(LATCH_PIN, OUTPUT);
 
+	digitalWrite(CS_DAC0, HIGH);
+	digitalWrite(CS_DAC1, HIGH);
 	digitalWrite(LATCH_PIN, HIGH);
 
-	attachInterrupt(digitalPinToInterrupt(2), pulseA, RISING);
-	attachInterrupt(digitalPinToInterrupt(3), pulseB, RISING);
+	pinMode(13, OUTPUT);
+	digitalWrite(13, LOW);
 
-	dac0.setSPIDivider(SPI_CLOCK_DIV4);
-	dac1.setSPIDivider(SPI_CLOCK_DIV4);
+	Serial.begin(115200);
 
+	//attachInterrupt(digitalPinToInterrupt(10), pulseA, RISING);
+	//attachInterrupt(digitalPinToInterrupt(12), pulseB, RISING);
+
+	SPI.begin();
+
+	dac0.setSPIDivider(SPI_CLOCK_DIV16);
+	dac1.setSPIDivider(SPI_CLOCK_DIV16);
+
+	
+
+	analogReadRes(16);
 	lastTime = micros();
+	APulses = 0;
+	BPulses = 0;
 }
 
 
@@ -45,16 +61,16 @@ void loop()
 	// By math, we get 100 pulses every 5ms at 3000 rpm.  That's 1/4 of a turn of the motors, which is great resolution.
 	if (APulses >= 100 || BPulses >= 100 || (thisTime - lastTime >= 5000))
 	{
-		APulses *= 150000;
-		BPulses *= 150000;
+		APulses *= ENC_TO_RPM;
+		BPulses *= ENC_TO_RPM;
 
 		// High Pass Filter, x = e^(-1/2) - e.g. it takes 2 samples for x to decay to 36.68% of its initial value
 		leftRPM = (long)((double)(.60*leftRPM) + .40*APulses / (thisTime - lastTime));
 		rightRPM = (long)((double)(.60*rightRPM) + .40*BPulses / (thisTime - lastTime));
-
-		lastTime = micros();
+		
 		APulses = 0;
 		BPulses = 0;
+		lastTime = micros();
 	}
 
 	// Read Throttle && steering pots once every millisecond (+ .1ms / analogRead)
@@ -63,25 +79,42 @@ void loop()
 		// Read the throttle and steering potentiometers, pass them through a simple filter.
 		throttlePos = .60*throttlePos + .40*analogRead(THROTTLE_PIN);
 		steeringPos = .60*steeringPos + .40*analogRead(STEERING_PIN);
-
+		
 		/*
 		*  Generate a linear correlation to steering angle and motor speed as a ratio 0-1.
 		*  This should be replaced with actual Ackermann steering values when implemented on the car.
 		*/
-		double leftSteer = constrain((steeringPos + 1) / 512.0, 0, 1);
-		double rightSteer = constrain((1024 - (steeringPos + 1)) / 512.0, 0, 1);
+		leftSteer = constrain((steeringPos) / 32768.0, 0, 1);
+		rightSteer = constrain((65535 - (steeringPos)) / 32768.0, 0, 1);
 
 		// Calculate the value to send to the DAC, multiplied by the steering ratio.
-		leftThrottle = (double)throttlePos / 1024.0 * 4096 * leftSteer;
-		rightThrottle = (double)throttlePos / 1024.0 * 4096 * rightSteer;
+		leftThrottle = (double)throttlePos / 65536.0 * 4096 * leftSteer;
+		rightThrottle = (double)throttlePos / 65536.0 * 4096 * rightSteer;
+
+		Serial.print("leftThrottle: ");
+		Serial.print(leftThrottle);
+		Serial.print("\trightThrottle: ");
+		Serial.println(rightThrottle);
 
 		// Write to the DACs
 		dac0.output(leftThrottle);
 		dac1.output(rightThrottle);
 
+		/*uint16_t out = (0 << 15) | (1 << 14) | (1 << 13) | (1 << 12) | (leftThrottle);
+		digitalWriteFast(CS_DAC0, LOW);
+		SPI.transfer(out >> 8);
+		SPI.transfer(out & 0xFF);
+		digitalWriteFast(CS_DAC0, HIGH);
+
+		out = (0 << 15) | (1 << 14) | (1 << 13) | (1 << 12) | (rightThrottle);
+		digitalWriteFast(CS_DAC1, LOW);
+		SPI.transfer(out >> 8);
+		SPI.transfer(out & 0xFF);
+		digitalWriteFast(CS_DAC1, HIGH);*/
+
 		// Quickly latch the DAC output by writing directly to the registers (Uno pin 6)
-		PORTD &= _BV(LATCH_PIN);
-		PORTD |= _BV(LATCH_PIN);
+		digitalWriteFast(LATCH_PIN, LOW);
+		digitalWriteFast(LATCH_PIN, HIGH);
 	}
 }
 
--- code/TestMule/__vm/.TestMule.vsarduino.h
+++ code/TestMule/__vm/.TestMule.vsarduino.h
@@ -5,17 +5,18 @@
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Arduino Uno, Platform=avr, Package=arduino
+	Hardware: Teensy 3.1, Platform=avr, Package=teensy
 */
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __AVR_ATmega328p__
-#define __AVR_ATmega328P__
-#define F_CPU 16000000L
-#define ARDUINO 165
-#define ARDUINO_AVR_UNO
+#define __MK20DX256__
+#define TEENSYDUINO 124
+#define ARDUINO 163
+#define F_CPU 96000000
 #define ARDUINO_ARCH_AVR
+#define USB_SERIAL
+#define LAYOUT_US_ENGLISH
 #define __cplusplus
 #define __inline__
 #define __asm__(x)
@@ -66,7 +67,6 @@ extern "C" void __cxa_pure_virtual() {;}
 
 
 #include <arduino.h>
-#include <pins_arduino.h> 
 #undef F
 #define F(string_literal) ((const PROGMEM char *)(string_literal))
 #undef PSTR
